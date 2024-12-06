d5f34b983019cc7f5dd38ea4de08e41477bf5ce0
Torjus Tveten <Torjus24sfj@gmail.com>
2024-12-06 12:27:08 +0100

    events

---
+++ script.js
@@ -0,0 +1,59 @@
+//! Click button to add to counter
+// const button = document.getElementById('my-button')
+// const counterDisplay = document.getElementById('counter-display')
+// let counter = 0;
+
+// button.addEventListener('click', increaseCounter)
+
+// function increaseCounter(){
+//     counter++
+//     counterDisplay.innerText = counter;
+// }
+
+//! Toggle darkmode (click)
+// const button = document.getElementById('darkModeToggle')
+
+// button.addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode')
+//     button.classList.toggle('button-dark')
+// })
+
+//! Toggle text (click)
+// const element = document.getElementById('duck')
+
+// element.addEventListener('click', () => {
+//     document.getElementById('duck').innerHTML = "Here is a duck"
+// })
+
+//! Keyboard event (keydown)
+// document.addEventListener('keydown', e => {
+//     console.log(`Key pressed ${e.key}`)
+// })
+
+//! Mouse event (mouseenter/mouseleave, click)
+// const box = document.getElementById('box')
+// const button = document.getElementById('reset')
+// const colortext = document.getElementById('colortext')
+
+// function updateColortext(color) // Color is a placeholder variable. #colortext call function and pass 'yellow' as the argument
+// {
+//     colortext.textContent = color;
+// }
+
+// box.addEventListener('mouseenter', e => {
+//     box.style.backgroundColor = 'yellow';
+//     updateColortext('yellow');
+// })
+
+// box.addEventListener('mouseleave', e => {
+//     box.style.backgroundColor = 'blue';
+//     updateColortext('blue');
+// })
+
+// button.addEventListener('click', e => {
+//     box.style.backgroundColor = 'red';
+//     updateColortext('red');
+// })
+
+//! Form event (submit, change, input, focus/blur)
+const 
